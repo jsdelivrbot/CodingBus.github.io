943740e291ab9d9e34539a31eb4c8a15c2cbbbee
Dingus <devindperez@gmail.com>
2018-03-09 15:14:42 -0800

    love u

--- heart.js
+++ heart.js
@@ -1,11 +1,27 @@
+var Cuties_Caps
+function preload() {
+  Cuties_Caps = loadFont("https://cdn.rawgit.com/CodingBus/CodingBus.github.io/3657d31b/CutiesCaps.otf")
+}
+
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+	textFont(Cuties_Caps)
+	createCanvas(displayWidth,displayHeight);
+	background(100)
 }
-
+var x;
+var y;
+var sz = 50;
 function draw() {
-	fill(180,30,60)
-	textSize(40);
-	textStyle(NORMAL);
-	text('Love You',displayHeight/2,displayWidth/2)
+	fill(180,30,60);
+	textSize(100);
+	//textAlign(CENTER,CENTER)
+	x = width/2;
+	y = height/2;
+	text('L',x,y);
+	text('0',x+sz,y);
+	text('v',x+sz*2,y);
+	text('e',x+sz*3,y);
+	text('uU',x+sz*4,y+sz*2);
 
 }
+
