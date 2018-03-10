4a3b17fef6dcb3add251d816d425c6b0a99efd93
dingus <devindperez@gmail.com>
2018-03-09 18:17:39 -0800

    Update heart.js

--- heart.js
+++ heart.js
@@ -1,9 +1,11 @@
 function setup() {
   createCanvas(displayWidth, displayHeight);
   background(80,0,50);
+  noStroke();
 }
-
+var x = 0;
 function draw() {
+  x++;
   fill(mouseX%255,20,mouseX%255+random(100),random(100,255);
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, x%60,x%60)
 }
